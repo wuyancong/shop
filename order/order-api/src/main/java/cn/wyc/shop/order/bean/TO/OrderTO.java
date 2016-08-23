594a092d7cbf39b5fe663ebf27620281dbe40e2d
wuyancong <641654237@qq.com>
2016-08-23 22:48:47 +0800

    commit something

---
+++ order/order-api/src/main/java/cn/wyc/shop/order/bean/TO/OrderTO.java
@@ -0,0 +1,19 @@
+package cn.wyc.shop.order.bean.TO;
+
+import java.io.Serializable;
+
+/**
+ * This bean is for caller to fill parameters.
+ * @author wuyancong
+ * @date 2016Äê7ÔÂ8ÈÕ
+ * @description 
+ */
+public class OrderTO implements Serializable {
+	
+	private static final long serialVersionUID = -8343851872826788617L;
+
+	private String productId;
+	private String productVersion;
+	private String customerId;
+	
+}
---
+++ order/order-api/src/main/java/cn/wyc/shop/order/service/orderService.java
@@ -0,0 +1,5 @@
+package cn.wyc.shop.order.service;
+
+public interface orderService {
+
+}
